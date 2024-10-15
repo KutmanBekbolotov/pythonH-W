3f5f2474047908496f8c5eb567ea6a8f1b2da677
KutmanBekbolotov <kutmanbekbolotov2003@gmail.com>
2024-10-15 22:13:18 +0600

    added git commands

--- main2.py
+++ main2.py
@@ -1,2 +1,15 @@
 print ('hello world')
 print ('Our project')
+
+
+#git pull origin BranchName(takes all changes)
+#git push origin BranchName(gives cheges)
+
+#git add .
+#git commit -m "decription"
+#git push origin BrachName
+
+#git config --global user.name"antago7"
+#git config --global user.email"@gmai.com"
+
+#git checkout BranchName(switch branch)
