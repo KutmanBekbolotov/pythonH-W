d536af682e763fd6175edacee79988a87a1f95a7
antago7 <restrong7@gmail.com>
2024-10-17 11:07:32 -0500

    added script

--- gittBrachWorks/lesson1.py
+++ gittBrachWorks/lesson1.py
@@ -1,3 +1,22 @@
+num = int(input('enter your number'))
+if num ==1:
+    print ('one')
+elif num ==2:
+    print ('two')
+elif num ==3:
+    print('three')
+elif num == 4:
+    print('four')
+else:
+    print ('error')
+
+
+
+
+
+
+
+
 #add here your tasks
 
 #если захотите запустить код который тут напишите,
