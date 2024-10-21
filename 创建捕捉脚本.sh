206bcb359e739580c4a96aa280a577a91a06699f
KutmanBekbolotov <kutmanbekbolotov2003@gmail.com>
2024-10-21 21:37:55 +0600

    js folder

--- 创建捕捉脚本.sh
+++ 创建捕捉脚本.sh
@@ -18,7 +18,7 @@ LOAD=`uptime | gawk '{print $NF}'`
 FREE=`vmstat 1 2 | sed -n '/[0-9]/p' | sed -n '2p' | gawk '{print $4}'`
 IDLE=`vmstat 1 2 | sed -n '/[0-9]/p' | sed -n '2p' | gawk '{print $15}'`
 #
-##########################################
+############################################################
 #
 echo "$DATE,$TIME,$USERS,$LOAD,$FREE,$IDLE" >> $REPORT_FILE
 #
