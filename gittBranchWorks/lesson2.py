9476e8b9d31246ba51dac15c41510a63e0c6093a
antago7 <restrong7@gmail.com>
2024-10-22 10:17:10 -0500

    branch fixed

---
+++ gittBranchWorks/lesson2.py
@@ -0,0 +1,11 @@
+number = int (input ('enter your number'))
+if number >0:
+    print ('integer')
+    if number %2==0:
+        print ('even')
+    else:
+        print('odd')
+elif number < 0:
+    print ('error')
+
+    
---
+++ gittBranchWorks/lesson3.py
@@ -0,0 +1,13 @@
+def number (a, b):
+    a=5
+    b=5
+    result= a + b
+    return result
+
+def call_func(func):
+    return func(0, 0)
+
+print(call_func(number)) 
+print("hello")
+
+
